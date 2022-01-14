e8e5ccadf0545bfccc37a5d925aad3b623a8cb3d
Bak David <bak.david96@gmail.com>
2022-01-15 00:33:14 +0200

    Adding modal to display error and dismiss functionality for modal

---
+++ src/UI/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./ErrorModal.module.css";
+import Card from "./Card";
+import Button from "./Button";
+
+export default function ErrorModal(props) {
+  return (
+    <div>
+      <div onClick={props.onConfirm} className={classes.backdrop}></div>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+}
