176f137315ab78579ab2090ec66d45e2313957c7
Bak David <bak.david96@gmail.com>
2022-01-14 23:42:06 +0200

    Setting className for custom component and setting its value

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
 
-export default function AddUser() {
+export default function AddUser(props) {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
 }
